550310c5a8f1c8a39804abdfa1324f72504786c7
Felipe Nascimento <felipen@animaeducacao.com.br>
2019-07-17 23:02:36 -0300

    subindo somente o arquivo de VAGAS

---
+++ RESTAPITH/Model/Jobs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPITH.Model
+{
+    /// <summary>
+    /// Vagas - que o empregador vai divulgar
+    /// obs: idVagas deve ser o mesmo Id dentro de jobTitles
+    /// </summary>
+    public class Jobs
+    {
+        public long IdVagas { get; set; }
+        public string strDescricao { get; set; }
+        public string strObservacoes { get; set; }
+        public DateTime dtDivulgacao { get; set; }
+        public DateTime dtCriacao { get; set; }
+    }
+}
